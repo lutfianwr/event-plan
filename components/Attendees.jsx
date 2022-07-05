26a2757b6f4d69e1851ddb6459de4862e404a831
Lutfi Nuril <lutfi.nurilanwar@gmail.com>
2022-07-05 16:05:49 +0700

    add detail & comment section

---
+++ components/Attendees.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Attendees() {
+  return (
+    <div>
+      <div className=" text-black p-3 rounded text-center py-8">
+        <div className="flex justify-center mb-2">
+          <img
+            src="https://i.imgur.com/zQZSWrt.jpg"
+            className="rounded-full max-h-24 max-w-24 flex items-center justify-center"
+          />
+        </div>
+
+        <h1 className="text-sm">Sarah</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Attendees;
