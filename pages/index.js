890f0f7a76ca787e4ea87d61c64abb0eead19a79
Lutfi Nuril <lutfi.nurilanwar@gmail.com>
2022-07-06 22:07:05 +0700

    register api

--- pages/index.js
+++ pages/index.js
@@ -3,64 +3,57 @@ import Image from "next/image";
 import Hero from "../components/Hero";
 import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
+import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from "../components/Card";
+import { useRouter } from "next/router";
 
 export default function Home(props) {
-  // const [event, setEvent] = useState([]);
+  const [event, setEvent] = useState([]);
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
-  const event = props.data;
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // const fetchProduct = async () => {
-  //   await axios
-  //     .get(
-  //       `
-  //       https://my-json-server.typicode.com/Maruta45/mockjson/events
-  //     `
-  //     )
-  //     .then((response) => {
-  //       // handle success
-
-  //       console.log(response.data);
-  //       const results = response.data;
-  //       setEvent(results);
-  //     })
-  //     .catch(function (error) {
-  //       // handle error
-  //       console.log(error);
-  //     })
-  //     .finally(() => setLoading(false));
-  // };
+  const fetchData = async () => {
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("http://3.86.179.206:80/events", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setEvent(data.data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   return (
     <div className={styles.container}>
       <Layout>
         <Hero />
+        {console.log(event.data)}
         <div className="grid grid-flow-row auto-rows-max grid-cols-2 md:grid-cols-4 lg:grid-cols-4 m-2 gap-3">
           {event.map((item) => (
             <Card
               key={item.id}
-              title={item.event.name}
-              location={item.event.location}
-              image={item.event.image}
-              date={item.event.date}
-              onClickItem={() => navigate(`detail/${item.id}`)}
+              title={item.event_name}
+              location={item.category}
+              image={item.image}
+              date={item.date}
+              onClickItem={() => router.push(`detail/${item.id}`)}
             />
           ))}
         </div>
       </Layout>
     </div>
   );
-}
-export async function getStaticProps() {
-  const response = await fetch(
-    `https://my-json-server.typicode.com/Maruta45/mockjson/events`
-  );
-  const data = await response.json();
-  return {
-    props: {
-      data,
-    },
-  };
 }
